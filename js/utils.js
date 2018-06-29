e608738908c9bacfbaaf2cec54e925c59f6f3809
Maksim Ko <Ramax13@gmail.com>
2018-06-29 12:51:22 +0300

    Добавление исправлений

--- js/utils.js
+++ js/utils.js
@@ -6,14 +6,14 @@
   };
 
   var randomArray = function (array, count) {
-    array.sort(function () {
-      return Math.random() - 0.5;
-    });
     var items = [];
     for (var i = 0; i < count; i++) {
       var item = array[i];
       items.push(item);
     }
+    array.sort(function () {
+      return Math.random() - 0.5;
+    });
     return items;
   };
 
